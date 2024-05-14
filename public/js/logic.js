dcda4dd758f47f0a5aaee30bbaa811bbec4e7c48
Andrew Bollard <bollard92@gmail.com>
2024-05-14 14:53:41 -0500

    pushit it real good

---
+++ public/js/logic.js
@@ -0,0 +1,13 @@
+//  todo 
+//     -add a function that populates the completed issue form into the homepage.handlebars.
+
+//     -add a filter function that will prevent tenants from viewing issues unrealated to their id.a
+
+//     -add function to login and log out account
+
+//     -add functions to submit issue.
+
+
+
+
+ 
