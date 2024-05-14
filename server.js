899f49fc5ab3a8feb75f0c250515df299b770870
whougie <whougie.c.lo@gmail.com>
2024-05-14 00:13:34 -0500

    adding routes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./controllers');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded( { extended: true}));
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Now listening http://localhost:${PORT}`));
