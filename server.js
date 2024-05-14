ffe9dd9b088e974d8cac91ef8923f7358a36c422
whougie <whougie.c.lo@gmail.com>
2024-05-14 12:48:22 -0500

    Update with server.js

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const routes = require('./controllers');
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const sessionConfigs = {
+  secret: 'Super duper Changed Secret key',
+  cookie: {
+    maxAge: 8 * 60 * 60 * 1000 // expires after 8 hours
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sessionConfigs));
+
+const hbs = exphbs.create({});
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded( { extended: true}));
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`Now listening http://localhost:${PORT}`));
+sequelize.sync({ force: false })
+.then( () => {
+  app.listen(PORT, () => console.log(`Now listening http://localhost:${PORT}`));
+})
