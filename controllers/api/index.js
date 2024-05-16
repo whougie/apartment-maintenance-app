5600e6c46d6f5a9a33af474defb24deb51fb8baa
gwiyomiamy <octipuss@gmail.com>
2024-05-16 10:40:36 -0700

    added apartment route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const accountRoute = require('./account-routes');
 const issuesRoute = require('./issues-routes')
+const apartmentRoute = require('./apartment-routes')
 
 router.use('/account', accountRoute);
 router.use('/issues', issuesRoute);
+router.use('/apartment', apartmentRoute);
 
 module.exports = router;
