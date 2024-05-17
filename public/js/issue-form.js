72c88b75532b7cf4f9b9c83afc366fea640cf9db
Andrew Bollard <bollard92@gmail.com>
2024-05-17 12:22:06 -0500

    added event delegation

--- public/js/issue-form.js
+++ public/js/issue-form.js
@@ -6,27 +6,29 @@ const submitMessage = document.querySelector('.submit-message')
 
 async function formPopulate(event) {
     event.preventDefault();
-    try {
-        const room = document.querySelector('#room').value
-        const issue = document.querySelector('#broken-thing').value
-        console.log(issue, room)
-
-        const response = await fetch('/api/issues', {
-            method: 'POST',
-            body: JSON.stringify({ issue, room }),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        if (response.ok) {
-            submitMessage.textContent = "Issue submitted! Return to homepage to view progress!"
-        } else {
-            throw new Error("error")
+    if(event.target===buttonSubmit) {
+        try {
+            const room = document.querySelector('#room').value
+            const issue = document.querySelector('#broken-thing').value
+            console.log(issue, room)
+            
+            const response = await fetch('/api/issues', {
+                method: 'POST',
+                body: JSON.stringify({ issue, room }),
+                headers: { 'Content-Type': 'application/json' }
+            })
+            if (response.ok) {
+                submitMessage.textContent = "Issue submitted! Return to homepage to view progress!"
+            } else {
+                throw new Error("error")
+            }
+        } catch (error) {
+            submitMessage.textContent = "Your apartment isn't the only thing on fire!"
         }
-    } catch (error) {
-        submitMessage.textContent = "Your apartment isn't the only thing on fire!"
     }
 }
 
 const buttonSubmit = document.querySelector('.submit-form')
 
 
-buttonSubmit.addEventListener('click', formPopulate);
+buttonSubmit.addEventListener('submit', formPopulate);
