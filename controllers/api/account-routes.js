14fd2fa97801f570330f260ab5d3b6124aa9f48d
ozgeahunbay <ahunb001@umn.edu>
2024-05-17 10:46:01 -0500

    all of the stuiff we lost yest4rday

--- controllers/api/account-routes.js
+++ controllers/api/account-routes.js
@@ -5,7 +5,7 @@ const rwc = require("random-weighted-choice");
 ////
 // Routes to retrieve from the DB or save data to it and returns a response with JSON
 ////
-router.get("/", async (req, res) => {
+router.get("/tenant", async (req, res) => {
   try {
     const tenantData = await Tenant.findAll({
       include: [{ model: Apartment }],
@@ -19,7 +19,7 @@ router.get("/", async (req, res) => {
   }
 })
 
-router.get("/:id", async (req, res) => {
+router.get("/tenant/:id", async (req, res) => {
   try {
     const result = await Tenant.findByPk(req.params.id)
     res.status(200).json(result);
@@ -28,13 +28,13 @@ router.get("/:id", async (req, res) => {
     res.status(400).json( err )
   }
 })
-router.post("/login", async (req, res) => {
+router.post("/tenant/login", async (req, res) => {
   let emailCheck
   
   try {
     emailCheck = await Tenant.findOne({
       where: {
-        email: req.body.email
+        tenant_email: req.body.tenant_email
       }
     })
   } catch(err){
@@ -46,10 +46,10 @@ router.post("/login", async (req, res) => {
   }
   
   // if we are this far, then the email matched
-  const hashedPassword = emailCheck.password
+  const hashedPassword = emailCheck.tenant_password
   
   // time to verify the hashed password 
-  const verified = await bcrypt.compare(req.body.password, hashedPassword)
+  const verified = await bcrypt.compare(req.body.tenant_password, hashedPassword)
   
   if( verified ){
     res.status(200).json({ status: "success" })
@@ -60,7 +60,7 @@ router.post("/login", async (req, res) => {
 })
 
 
-router.post("/", async (req, res) => {
+router.post("/tenant", async (req, res) => {
   try {
     const result = await Tenant.create(req.body)
     res.json({ status: "success", payload: result })
@@ -70,62 +70,126 @@ router.post("/", async (req, res) => {
 })
 
 
-
-router.post('/', async (req, res) => {
-  console.log(req.body); // TODO: Temporary outputting the request from the form of a new account being created until more of the project is completed, which will save the account information of user to DB
+router.get("/manager", async (req, res) => {
   try {
-    res.json({status: "Great and POST account successfully created"});
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({status: Failed, payload: error.message})
-  }
-})
-
-router.post('/login', async (req, res) => {
-  // TODO need to check to see if password for the username is correct
-  console.log(req.body);
-  try {
-    res.json({status: "Got the POST Login successfully"});
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({status: Failed, payload: error.message})
-  }
-})
-
-router.put('/', async (req, res) => {
-  // TODO need make change to the db to update account appt changes
-  console.log(req.body);
-  try {
-    res.json({status: "Got the PUT Login successfully"});
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({status: Failed, payload: error.message})
-  }
-})
-
-
-router.post('/issue', async (req, res) => {
-  // TODO need to create a new issue and save in DB
-  console.log(req.body);
-  try {
-    res.json({status: "Got the POST issue successfully"});
-  } catch (error) {
-    res.status(500).json({status: Failed, payload: error.message})
-  }
-})
-
-router.get('/issue/:id', async (req, res) => {
-  // TODO need to create get all the issues in respect to request from the  DB
-  console.log(req.body);
-  try {
-    const issuesData = await Issue.findByPk(req.params.id, {
-      include: [Manager, Tenant, Handyman]
+    const managerData = await Manager.findAll({
+      
     })
-    res.json(issuesData);
-  } catch (error) {
-    res.status(500).json({status: Failed, payload: error.message})
+    console.log(managerData)
+    res.status(200).json(managerData);
+    
+  } catch(err){ 
+    console.log(err)
+    res.status(400).json( err )
   }
 })
+
+router.get("/manager/:id", async (req, res) => {
+  try {
+    const result = await Manager.findByPk(req.params.id)
+    res.status(200).json(result);
+  } catch(err){
+    console.log(err)
+    res.status(400).json( err )
+  }
+})
+router.post("/manager/login", async (req, res) => {
+  let emailCheck
+  
+  try {
+    emailCheck = await Manager.findOne({
+      where: {
+        manager_email: req.body.manager_email
+      }
+    })
+  } catch(err){
+    res.status(400).json({ status: "error" })
+  }
+  
+  if( !emailCheck ){
+    return res.status(401).json({ status: "error", msg: 'incorrect email'})
+  }
+  
+  // if we are this far, then the email matched
+  const hashedPassword = emailCheck.manager_password
+  
+  // time to verify the hashed password 
+  const verified = await bcrypt.compare(req.body.manager_password, hashedPassword)
+  
+  if( verified ){
+    res.status(200).json({ status: "success" })
+  } else {
+    res.status(401).json({ status: "error", msg: 'incorrect password' })
+  }
+  
+})
+
+
+router.post("/manager", async (req, res) => {
+  try {
+    const result = await Manager.create(req.body)
+    res.json({ status: "success", payload: result })
+  } catch(err){
+    res.status(400).json({ status: "error" })
+  }
+})
+
+
+// router.post('/', async (req, res) => {
+//   console.log(req.body); // TODO: Temporary outputting the request from the form of a new account being created until more of the project is completed, which will save the account information of user to DB
+//   try {
+//     res.json({status: "Great and POST account successfully created"});
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({status: Failed, payload: error.message})
+//   }
+// })
+
+// router.post('/login', async (req, res) => {
+//   // TODO need to check to see if password for the username is correct
+//   console.log(req.body);
+//   try {
+//     res.json({status: "Got the POST Login successfully"});
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({status: Failed, payload: error.message})
+//   }
+// })
+
+// router.put('/', async (req, res) => {
+//   // TODO need make change to the db to update account appt changes
+//   console.log(req.body);
+//   try {
+//     res.json({status: "Got the PUT Login successfully"});
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({status: Failed, payload: error.message})
+//   }
+// })
+
+
+// router.post('/issue', async (req, res) => {
+//   // TODO need to create a new issue and save in DB
+//   console.log(req.body);
+//   try {
+//     res.json({status: "Got the POST issue successfully"});
+//   } catch (error) {
+//     res.status(500).json({status: Failed, payload: error.message})
+//   }
+// })
+
+// router.get('/issue/:id', async (req, res) => {
+//   // TODO need to create get all the issues in respect to request from the  DB
+//   console.log(req.body);
+//   try {
+//     const issuesData = await Issue.findByPk(req.params.id, {
+//       include: [Manager, Tenant, Handyman]
+//     })
+//     res.json(issuesData);
+//   } catch (error) {
+//     res.status(500).json({status: Failed, payload: error.message})
+//   }
+// })
 
 
 // router.get('/issues', async (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -13,11 +13,13 @@ const seedDatabase = async () => {
 
   const apartmentData = await Apartment.bulkCreate(apartmentsData)
   const apartments = apartmentData.map(apartment => apartment.get({ plain: true }))
-
-  const tenantData = await Tenant.bulkCreate(tenantsData.map((tenant, index) => { return { ...tenant, apt_id: apartments[index].id } }));
+  
+  const tData = tenantsData.map((tenant, index) => { return { ...tenant, apt_id: apartments[index].id } })
+  const tenantData = await Tenant.bulkCreate(tData, { individualHooks: true });
   const tenants = tenantData.map(tenant => tenant.get({ plain: true }))
 
-  const managerData = await Manager.bulkCreate(managersData);
+  const mData = managersData.map((manager) => { return { ...manager } })
+  const managerData = await Manager.bulkCreate(mData, { individualHooks: true} );
   const managers = managerData.map(manager => manager.get({ plain: true }))
 
   const handymanData = await Handyman.bulkCreate(handymansData);
