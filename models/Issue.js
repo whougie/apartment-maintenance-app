66ffa72b82a90eb725c4d3461003d0473e3dec73
Andrew Bollard <bollard92@gmail.com>
2024-05-17 13:45:13 -0500

    deleted schedule date from issue model

--- models/Issue.js
+++ models/Issue.js
@@ -37,10 +37,7 @@ Issue.init(
                 key: 'id'
             }
         },
-        date_scheduled: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+        
         handyman_id: {
             type: DataTypes.INTEGER,
             references: {
