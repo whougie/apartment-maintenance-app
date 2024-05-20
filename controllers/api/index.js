6bd00a4e1c5c4db4731e92249a4a6420a056b720
Andrew Bollard <bollard92@gmail.com>
2024-05-19 19:06:28 -0500

    seeds render update

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,14 @@ const router = require('express').Router();
 const accountRoute = require('./account-routes');
 const issuesRoute = require('./issues-routes')
 const apartmentRoute = require('./apartment-routes')
-const handymanRoute = require('./handyman-routes')
-// const tenantRoutes = require("./tenant-routes")
+const handymanRoute = require('./handyman-routes');
+const seedRoute = require('./seed-routes');
+
 
 router.use('/account', accountRoute);
 router.use('/issues', issuesRoute);
 router.use('/apartment', apartmentRoute);
 router.use('/handyman', handymanRoute);
-// router.use('/tenant', tenantRoutes);
+router.use('/seeds', seedRoute)
 
 module.exports = router;
